51258e81ffc422273a3057fcea771a88b986ee51
Vivianne Christi <viviannec.mferreira@gmail.com>
2024-01-22 16:28:43 -0300

    Aula 1 (22/01/24)
    Inicio Js.
    Aprendizagem de Variável, utilização de if, else if

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-document.write('LIBBS 2024');
+/*document.write('LIBBS 2024');
 
-var nome;
-var endereco;
-var telefone;
+var nome = 'Vivianne Christi';
+var endereco = 'Rua da minha casa';
+var telefone = '11 4002-8922';
 const anoNascimento = 2005;
+
+//nome = prompt('Qual o seu nome?');
+
+studing =confirm('Está estudando no momento?');
+console.log(studing);
+
+
+if(studing){
+    alert('Parabéns');
+}else{
+    alert('Vamos Estudar...');
+}
+
+document.write(nome + '<br>');
+document.write(endereco + '<br>');
+document.write(telefone + '<br>');
+document.write(anoNascimento + '<br>');
+
+console.log('Estou aqui...')
+
+alert('Você chegou aqui');*/
+
+//ATIVIDADE 1
+
+/*O usuarío digita a idade e:
+12=>alerta que ainda brinca
+18=>Parabéns! hora de tirar a carta!
+>25 Já não precisar morar com papai
+>50 =>Nada de neto?
+*/
+
+
+var idade;
+
+idade = prompt('Qual é sua idade?');
+
+if (idade <=17){
+    alert('Novo demais, vai brincar!');
+}else if (idade >=18 && idade <=24){
+    alert('Parabéns pode tirar a carta!');
+}else if( idade >=25 && idade <=49){
+    alert('Já não precisar morar com papai');
+}else if(idade >=50){
+    alert('Nada de Neto?');
+}
